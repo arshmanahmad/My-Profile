61a89e31908bc048d4e33d27805d22ffddfd9142
arshmanahmad <arshmanmuhammad153@gmail.com>
2025-10-18 23:49:29 +0500

    feat: indentation update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,6 @@ export default function Portfolio() {
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
   ]
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false)
