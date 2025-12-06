9c4779fd3ffb0a7e2ee9e114c3778ff2603f2434
arshmanahmad <arshmanmuhammad153@gmail.com>
2025-12-06 23:52:12 +0500

    feat: update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,7 @@ export default function Portfolio() {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1500);
+    
     const handleScroll = () => {
       const sections = navItems.map((item) => item.href.substring(1));
       const scrollPosition = window.scrollY + 100;
