4da549d2a63834a6cc5795045a970580a9acbcc0
arshmanahmad <arshmanmuhammad153@gmail.com>
2026-01-11 22:35:39 +0200

    feat: update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,6 @@ import { ClientReviews } from "../components/ClientReviews";
 export default function Portfolio() {
   const [activeSection, setActiveSection] = useState("home");
   const [isLoading, setIsLoading] = useState(true);
-
   const navItems = [
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
