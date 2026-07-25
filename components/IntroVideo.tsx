5854f5fee8684087aa278ee8e8fb27889f29241e
arshmanahmad <arshmanmuhammad153@gmail.com>
2026-07-25 23:02:07 +0400

    feat: indentation update

--- components/IntroVideo.tsx
+++ components/IntroVideo.tsx
@@ -16,7 +16,6 @@ const trustChips = [
 interface IntroVideoProps {
   video: IntroVideoData;
 }
-
 export function getInstagramEmbedUrl(reelUrl: string): string | null {
   const match = reelUrl.match(/\/reel\/([^/?]+)/);
   if (!match?.[1]) return null;
